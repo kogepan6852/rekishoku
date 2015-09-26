966d3dea90ee4b9a4d93bb3b7e05874db60e22b0
akihiro.tanaka <akihiro.tanaka@accenture.com>
2015-09-27 00:29:01 +0900

    単一継承テーブル対応

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922023348) do
+ActiveRecord::Schema.define(version: 20150926132631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",                      null: false
