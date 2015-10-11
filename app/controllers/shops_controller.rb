9cde0ffab38a85c9ef114bbb7048ca2260d0fb1c
堅固潤也 <junya.kengo@monstar-lab.com>
2015-10-12 00:27:30 +0900

    Geocodeの最新版

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -26,10 +26,10 @@ class ShopsController < ApplicationController
   # POST /shops.json
   def create
     # 住所から緯度経度を求める
-    Geocoder.configure(:language  => :ja,  :units => :km )
-    addressPlace = Geocoder.coordinates(shop_params[:address1]);
+    # Geocoder.configure(:language  => :ja,  :units => :km )
+    # addressPlace = Geocoder.coordinates(shop_params[:address1]);
     # 緯度経度を代入する
-    @shop = Shop.new(shop_params.merge(latitude: addressPlace[0], longitude: addressPlace[1]))
+    @shop = Shop.new(shop_params)
 
     respond_to do |format|
       if @shop.save
