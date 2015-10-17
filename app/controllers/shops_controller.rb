dee0db841dc49c289ebc96a13f9baa62d8f271a8
堅固潤也 <junya.kengo@monstar-lab.com>
2015-10-18 01:23:15 +0900

    距離を求める処理

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,13 +4,21 @@ class ShopsController < ApplicationController
   # GET /shops
   # GET /shops.json
   def index
-    @shops = Shop.all
+    latitudeRange = 0.00000901337 # 緯度計算の値
+    longitudeRange = 0.0000109664 # 経度計算の値
+    address = "島根県松江市秋鹿町３４１９−２" # 仮の住所
+    shopDistance = 10000 #shopからの距離[m]
+
+    #現在地を受け取るの緯度経度を求める
+    addressPlace = Geocoder.coordinates(address);
+    # 店舗フィルタをかかる
+    @shops = Shop.where('latitude >= ? AND longitude >= ? AND latitude <= ? AND longitude <= ?',addressPlace[0]-shopDistance*latitudeRange,addressPlace[1]-shopDistance*longitudeRange,addressPlace[0]+shopDistance*latitudeRange,addressPlace[1]+shopDistance*longitudeRange)
   end
 
   # GET /shops/1
   # GET /shops/1.json
   def show
-    @shops = Shop.all
+     #render json: Shop
   end
 
   # GET /shops/new
@@ -70,6 +78,6 @@ class ShopsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def shop_params
-       params.require(:shop).permit(:name, :description, :url, :image, :subimage, :image_quotation_url, :image_quotation_name, :post_quotation_url, :post_quotation_name, :address1, :address2, :latitude, :longitude, :menu)
+      params.require(:shop).permit(:name, :description, :url, :image, :subimage, :image_quotation_url, :image_quotation_name, :post_quotation_url, :post_quotation_name, :address1, :address2, :latitude, :longitude, :menu)
     end
 end
