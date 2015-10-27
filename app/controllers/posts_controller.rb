15c4d7bff2da9fe13897877547722a3de33aa0fd
堅固潤也 <junya.kengo@monstar-lab.com>
2015-10-28 01:48:56 +0900

    posts_constrllerのerrer対応

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,11 @@ class PostsController < ApplicationController
 
     # フリーワードとカテゴリ検索を行なう
     if params[:text] && params[:category]
-      @posts = @posts.where('title LIKE ? || content LIKE ? || spost_details LIKE ?  && category_id == ?', params[:text],params[:text],params[:text],params[:category]])
+      @posts = @posts.where('title LIKE ? || content LIKE ? || spost_details LIKE ?  && category_id == ?', params[:text],params[:text],params[:text],params[:category])
     elsif params[:text]
       @posts = @posts.where('title LIKE ? || content LIKE ? || spost_details LIKE ?', params[:text],params[:text],params[:text])
     elsif params[:category]
-      @posts = @posts.where('category_id == ?', params[:category]])
+      @posts = @posts.where('category_id == ?', params[:category])
     end
 
     render json: @posts
