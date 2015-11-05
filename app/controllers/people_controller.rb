9ab1b9b3ad13ba473514314e86301114b9b43d4c
吉本明弘 <yoshimotoakihiro@MacBook-Air.local>
2015-11-05 23:41:08 +0900

    人物と時代　紐付け（未完成）

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,6 @@
 class PeopleController < ApplicationController
   before_action :set_person, only: [:show, :edit, :update, :destroy]
+  before_action :set_periods, only: [:new, :edit]
 
   # GET /people
   # GET /people.json
@@ -19,13 +20,14 @@ class PeopleController < ApplicationController
 
   # GET /people/1/edit
   def edit
-    @periods = Period.all
+
   end
 
   # POST /people
   # POST /people.json
   def create
-    @person = Person.new(person_params)
+    @person = Person.new(person_params, people_period_params)
+
     respond_to do |format|
       if @person.save
         format.html { redirect_to @person, notice: 'Person was successfully created.' }
@@ -67,8 +69,17 @@ class PeopleController < ApplicationController
       @person = Person.find(params[:id])
     end
 
+    def set_periods
+      @periods = Period.all
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def person_params
       params.require(:person).permit(:name, :furigana)
     end
+
+    def people_period_params
+      params.require(:people_period).permit(:person_id, :period_id, :period_ids => [])
+    end
+
 end
