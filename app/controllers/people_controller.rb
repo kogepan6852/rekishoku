2e01ddd13bee5491fa2e5acd430461a42a0ae745
堅固潤也 <junya.kengo@monstar-lab.com>
2015-11-20 15:55:30 +0900

    人物表示可能

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,6 +13,10 @@ class PeopleController < ApplicationController
   # GET /people/api.json
   def api
     @people = Person.all
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @people }
+    end
   end
 
   # GET /people/1
