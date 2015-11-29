8264453b7be13e2e9875d1940b0eca149a6344b3
akihiro.tanaka <big.a.0816@gmail.com>
2015-11-30 01:16:53 +0900

    ライター一覧:表示不備修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    if !current_user
+    if !current_user || user_params
       user = {
         "user" =>{
           "id" => @user.id,
