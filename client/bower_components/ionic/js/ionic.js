111a4139abcf14a22a3aa2ffca23457db692078e
akihiro.tanaka <big.a.0816@gmail.com>
2015-12-01 01:28:52 +0900

    パフォーマンス対応

--- client/bower_components/ionic/js/ionic.js
+++ client/bower_components/ionic/js/ionic.js
@@ -1,8 +1,8 @@
 /*!
- * Copyright 2014 Drifty Co.
+ * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.1.0-nightly-1573
+ * Ionic, v1.1.1-nightly-1653
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.1.0-nightly-1573';
+window.ionic.version = '1.1.1-nightly-1653';
 
 (function (ionic) {
 
