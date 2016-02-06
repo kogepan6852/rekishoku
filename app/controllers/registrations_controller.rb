ebfb299077cc183351eae469000e1b4f7b51a17d
吉本明弘 <yoshimotoakihiro@MacBook-Air.local>
2016-02-07 03:23:57 +0900

    ログインなどした後のリダイレクト先の変更

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   prepend_before_filter :require_no_authentication, :only => [ :cancel]
   prepend_before_filter :authenticate_scope!, :only => [:new, :create ,:edit, :update, :destroy]
+
+  def after_sign_up_path_for(resource)
+    menu_index_path
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    menu_index_path
+  end
 end
