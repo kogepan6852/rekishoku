c2bf5e8279447926065ebdd378dd3310e51b4685
吉本明弘 <yoshimotoakihiro@MacBook-Air.local>
2016-02-08 23:10:25 +0900

    アカウント作成時の処理の変更

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,13 @@ class RegistrationsController < Devise::RegistrationsController
   prepend_before_filter :require_no_authentication, :only => [ :cancel]
   prepend_before_filter :authenticate_scope!, :only => [:new, :create ,:edit, :update, :destroy]
 
-  def after_sign_up_path_for(resource)
+  # アカウント作成時に強制的にログインさせないようにする
+  def sign_up(resource_name, resource)
     menu_index_path
   end
 
-  def after_inactive_sign_up_path_for(resource)
+  # アカウント作成時のリダイレクト先
+  def after_sign_up_path_for(resource)
     menu_index_path
   end
 end
