c32e13551edd3e2a4225a333778f162edb5f4d9f
吉本明弘 <yoshimotoakihiro@MacBook-Air.local>
2016-02-10 18:36:19 +0900

    modelのpostの部分を削除してたので、元に戻した。

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
+  has_many :posts, through: :people_posts
+  has_and_belongs_to_many :posts
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :periods
   has_and_belongs_to_many :shops
