8602caf1a20c40bc32e0557f5a8b061229f116f9
akihiro.tanaka <big.a.0816@gmail.com>
2016-03-19 01:03:54 +0900

    ionicテキストコピー対応バグ修正

--- client/bower_components/ionic/js/ionic.js
+++ client/bower_components/ionic/js/ionic.js
@@ -634,7 +634,7 @@ window.ionic.version = '1.1.1-nightly-1653';
     // its native behavior. this doesnt prevent the scrolling,
     // but cancels the contextmenu, tap highlighting etc
     // set to false to disable this
-    stop_browser_behavior: false
+    stop_browser_behavior: 'disable-user-behavior'
   };
 
   // detect touchevents
@@ -5300,15 +5300,12 @@ ionic.views.Scroll = ionic.views.View.inherit({
       var mousedown = false;
 
       self.mouseDown = function(e) {
-
-        return; // <--- This disables all mouseDown events from scrolling the page
-
         if ( ionic.tap.ignoreScrollStart(e) || e.target.tagName === 'SELECT' ) {
           return;
         }
         self.doTouchStart(getEventTouches(e), e.timeStamp);
 
-        if( !ionic.tap.isTextInput(e.target) ) {
+        if ( !ionic.tap.isTextInput(e.target) ) {
           e.preventDefault();
         }
         mousedown = true;
