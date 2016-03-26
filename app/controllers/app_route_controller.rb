efaaf2f2fbe34ddcf4d76f9280f2cbf0adfbd870
akihiro.tanaka <big.a.0816@gmail.com>
2016-03-26 22:40:06 +0900

    プレビュー表示不可バグ修正

--- app/controllers/app_route_controller.rb
+++ app/controllers/app_route_controller.rb
@@ -11,36 +11,38 @@ class AppRouteController < ApplicationController
       render :file => path, :layout => false
     else
       urls = url.split('/')
-      if urls[0] == 'post' && urls[1].present?
-        @post = Post.find(urls[1].to_s)
+      if urls[0] == 'post' && urls[1].present? && params[:preview] != 'true'
+        @post = Post.where("id = ? and status = ? and published_at <= ?", urls[1].to_s, 1, Date.today).limit(1)
 
-        keywords = Array.new
-        keywords.push("歴食")
-        @post.people.each do |person|
-          keywords.push(person.name)
-        end
-        @post.shops.each do |shop|
-          keywords.push(shop.name)
-        end
-        # SEO用metaタグ設定
-        set_meta_tags title: @post.title
-        set_meta_tags description: @post.content.gsub(/(\r\n|\r|\n|\f)/,"")
-        set_meta_tags keywords: keywords.join(",")
-        opg = {
-          title: @post.title,
-          type: 'website',
-          image: @post.image.md.url,
-          site_name: "歴食",
-          description: @post.content.gsub(/(\r\n|\r|\n|\f)/,""),
-          locale: 'ja_JP'
-        }
-        set_meta_tags open_graph: opg
+        if @post.present?
+          keywords = Array.new
+          keywords.push("歴食")
+          @post.people.each do |person|
+            keywords.push(person.name)
+          end
+          @post.shops.each do |shop|
+            keywords.push(shop.name)
+          end
+          # SEO用metaタグ設定
+          set_meta_tags title: @post.title
+          set_meta_tags description: @post.content.gsub(/(\r\n|\r|\n|\f)/,"")
+          set_meta_tags keywords: keywords.join(",")
+          opg = {
+            title: @post.title,
+            type: 'website',
+            image: @post.image.md.url,
+            site_name: "歴食",
+            description: @post.content.gsub(/(\r\n|\r|\n|\f)/,""),
+            locale: 'ja_JP'
+          }
+          set_meta_tags open_graph: opg
 
-        # ionic用jsを先に読み込んでおく
-        vendor = Dir.glob(Rails.root.to_s + "/public/scripts/vendor*.js")
-        scripts = Dir.glob(Rails.root.to_s + "/public/scripts/scripts*.js")
-        @vendor_js =  'http://' + request.host_with_port + '/scripts/' + File.basename(vendor[0])
-        @scripts_js =  'http://' + request.host_with_port + '/scripts/' + File.basename(scripts[0])
+          # ionic用jsを先に読み込んでおく
+          vendor = Dir.glob(Rails.root.to_s + "/public/scripts/vendor*.js")
+          scripts = Dir.glob(Rails.root.to_s + "/public/scripts/scripts*.js")
+          @vendor_js =  'http://' + request.host_with_port + '/scripts/' + File.basename(vendor[0])
+          @scripts_js =  'http://' + request.host_with_port + '/scripts/' + File.basename(scripts[0])
+        end
 
       # elsif urls[0] == 'shop' && urls[1].present?
       #   @shop = Shop.find(urls[1].to_s)
