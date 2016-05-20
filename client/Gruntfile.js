26a8f803e38237f6cfbc41b8e434ce20cdad673c
akihiro.tanaka <big.a.0816@gmail.com>
2016-05-20 09:01:18 +0900

    デプロイ用に微修正

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -164,7 +164,10 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/{,*/}*',
             '!<%= yeoman.dist %>/.git{,*/}*'
           ]
-        }]
+        }],
+        options: {
+          force: true
+        }
       },
       server: '.tmp'
     },
