bd63632c1e91dee40719add498a66b1a6164c38f
堅固潤也 <junya.kengo@monstar-lab.com>
2016-06-03 16:26:22 +0900

    １対１から　多対多に変更

--- app/models/feature_detail.rb
+++ app/models/feature_detail.rb
@@ -1,5 +1,5 @@
 class FeatureDetail < ActiveRecord::Base
-  belongs_to :feature
+  has_and_belongs_to_many :features
   has_and_belongs_to_many :external_links
   has_and_belongs_to_many :posts
   has_and_belongs_to_many :shops
