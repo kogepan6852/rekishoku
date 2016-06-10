07695f2cf07a29dcdb4a25dcf895c9724feb4a25
堅固潤也 <junya.kengo@monstar-lab.com>
2016-06-10 15:33:47 +0900

    外部リンクのDB修正

--- db/migrate/20160602084327_create_feature_details.rb
+++ db/migrate/20160602084327_create_feature_details.rb
@@ -6,12 +6,6 @@ class CreateFeatureDetails < ActiveRecord::Migration
       t.integer  "related_type",   default: 0,     null: false
       t.integer  "related_id",                     null: false
       t.integer  "order",          default: 0,     null: false
-      t.string   "external_link_title"
-      t.text     "content"
-      t.string   "image"
-      t.string   "quotation_url"
-      t.string   "quotation_name"
-      t.boolean  "is_external_link",               default: false
       t.timestamps                                 null: false
     end
   end
