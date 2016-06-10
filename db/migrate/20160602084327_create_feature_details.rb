5ed983037dcdd3a1e1426157a2fe29f79c42e698
堅固潤也 <junya.kengo@monstar-lab.com>
2016-06-10 17:41:28 +0900

    seedと特集詳細の対応

--- db/migrate/20160602084327_create_feature_details.rb
+++ db/migrate/20160602084327_create_feature_details.rb
@@ -3,8 +3,8 @@ class CreateFeatureDetails < ActiveRecord::Migration
     create_table :feature_details do |t|
       t.integer  "feature_id"
       t.string   "title",                          null: false
-      t.integer  "related_type",   default: 0,     null: false
-      t.integer  "related_id",                     null: false
+      t.stting   "related_type"
+      t.integer  "related_id",     default: 0
       t.integer  "order",          default: 0,     null: false
       t.timestamps                                 null: false
     end
