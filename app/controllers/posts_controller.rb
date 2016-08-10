b9fac4b685dcabdab085f62e4c7eba9f9d57cebb
堅固潤也 <junya.kengo@monstar-lab.com>
2016-08-10 16:19:42 +0900

    [管理画面]POSTの店舗紐付けがうまくいかない問題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,7 +63,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :image, :favorite_count, :status, :user_id, :quotation_url, :quotation_name, :category_id, :memo, :person_ids => [])
+      params.require(:post).permit(:title, :content, :image, :favorite_count, :status, :user_id, :quotation_url, :quotation_name, :category_id, :memo, :shop_ids => [], :person_ids => [])
     end
 
     def post_time_params
