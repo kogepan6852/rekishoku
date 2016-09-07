768a40e70ea0bc7cc51317ef5bb0e0567d40dd30
堅固潤也 <junya.kengo@monstar-lab.com>
2016-09-08 01:16:21 +0900

    少し制限を加える

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < ActionController::Base
 
   before_filter :allow_cross_domain_access
   def allow_cross_domain_access
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Allow-Origin'] = 'http://www.rekishoku.jp'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
