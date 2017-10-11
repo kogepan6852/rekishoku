c04b4a615718b671026ba8b1862c1aa363d62790
kogepan159 <kogepan6852@yahoo.co.jp>
2017-10-12 08:19:57 +0900

    時代が返却されるように対応

--- app/controllers/api_shops_controller.rb
+++ app/controllers/api_shops_controller.rb
@@ -81,7 +81,8 @@ class ApiShopsController < ApplicationController
   # GET /api/shops/1
   # 詳細データ表示
   def show
-    @shop = Shop.joins("LEFT OUTER JOIN shop_translations ON shops.id = shop_translations.shop_id").select('shops.*, shop_translations.name as period_name').find(params[:id])
+    @shop = Shop.joins("LEFT OUTER JOIN period_translations ON shops.period_id = period_translations.period_id")
+    .select('shops.*, period_translations.name as period_name').find(params[:id])
 
     render json: get_shop_json(@shop)
   end
