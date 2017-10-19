1882614eb0392c1c9343c3d74b12ce3a3d3beee4
kogepan159 <kogepan6852@yahoo.co.jp>
2017-10-20 00:24:03 +0900

    #1 story_relations対応

--- app/controllers/api_story_relations_controller.rb
+++ app/controllers/api_story_relations_controller.rb
@@ -23,7 +23,8 @@ class ApiStoryRelationsController < ApplicationController
       elsif story_relation[:related_type] == "Story"
 
         # 対応するstoryの情報を取得する
-        story = Story.joins(:category).select('stories.*, categories.id as category_id, categories.name as category_name, categories.slug as category_slug').find(story_detail[:related_id])
+        story = Story.joins(:category).joins("LEFT OUTER JOIN category_translations ON stories.category_id = category_translations.category_id")
+        .select('stories.*, categories.id as category_id, category_translations.name as category_name, categories.slug as category_slug').find(story_relation[:related_id])
 
         obj = get_story_json(story)
         # detail["story"] = obj
@@ -35,6 +36,7 @@ class ApiStoryRelationsController < ApplicationController
       end
 
       obj["story_detail_id"] = story_relation.story_detail_id
+      obj["order"] = story_relation.order
 
       rtnstoryDetails.push(obj)
     end
@@ -42,58 +44,6 @@ class ApiStoryRelationsController < ApplicationController
     render json: rtnstoryDetails
   end
 
-  # story /api/story_details
-  # def show
-  #   isSuccess = true
-  #   story_detail = params[:story_details]
-  #   params[:story_details].each_with_index do |story_detail, i|
-  #     if story_detail['id']
-  #       # 更新処理
-  #       @story_detail = StoryDetail.find(story_detail['id'])
-  #     else
-  #       # 新規作成処理
-  #       @story_detail = StoryDetail.new
-  #     end
-  #
-  #     @story_detail.story_id = story_detail['story_id']
-  #     @story_detail.title = story_detail['title']
-  #     @story_detail.content = story_detail['content']
-  #     @story_detail.quotation_url = story_detail['quotation_url']
-  #     @story_detail.quotation_name = story_detail['quotation_name']
-  #     @story_detail.is_eye_catch = story_detail['is_eye_catch']
-  #     @story_detail.related_type = story_detail['related_type']
-  #     @story_detail.related_id = story_detail['related_id']
-  #     if story_detail['image']
-  #       @story_detail.image = story_detail['image']
-  #     end
-  #     if !@story_detail.save
-  #       isSuccess = false
-  #     end
-  #
-  #   end
-  #
-  #   if isSuccess
-  #     render json: @story_detail, status: :created
-  #   else
-  #     render json: @story_detail.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # PATCH/PUT /api/story_details/1
-  # def update
-  #   if @story_detail.update(story_detail_params)
-  #     render json: @story_detail, status: :ok
-  #   else
-  #     render json: @story_detail.errors, status: :unprocessable_entity
-  #   end
-  # end
-  #
-  # # DELETE /api/story_details/1
-  # def destroy
-  #   id = params[:id]
-  #   StoryDetail.accessible_by(current_ability).destroy_all(['story_id = ?', id])
-  #   head :no_content
-  # end
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
